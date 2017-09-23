c8c43b12cff14e08c401a5282e8dae06f23c133d
Jarrod <jwmcenroe@gmail.com>
2017-09-22 22:36:50 -0700

    Moved win condition out of click function and moved jQuery load to the end of html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,102 @@
-// Setting up random numbers 1-12 for each crystal
-var blue;
-var green;
-var pink;
-var yellow;
-var userTotal = 0;
-var wins = 0;
-var losses = 0;
-var Random;
+$(document).ready(function() {
 
-// Resets the game
-function reset() {
-    Random
-    console.log(Random)
-    $('#randomNumber').html(Random);
-    blue = Math.floor(Math.random() * 12) + 1
-    green = Math.floor(Math.random() * 12) + 1
-    pink = Math.floor(Math.random() * 12) + 1
-    yellow = Math.floor(Math.random() * 12) + 1
-    userTotal = 0;
-    $('#finalTotal').html(userTotal);
-}
+           // Storing variables for later use in functions
+           var blue = Math.floor(Math.random() * 12) + 1
+           var green = Math.floor(Math.random() * 12) + 1
+           var pink = Math.floor(Math.random() * 12) + 1
+           var yellow = Math.floor(Math.random() * 12) + 1
+           var userTotal = 0;
+           var wins = 0;
+           var losses = 0;
+           var Random;
 
-// Selects a random number 19 - 120 to be shown at the start of the game 
-var Random = Math.floor(Math.random() * 120) + 19
+           // Resets the game
+           function reset() {
+               Random
+               console.log(Random)
+               $('#randomNumber').html(Random);
+               blue;
+               green;
+               pink;
+               yellow;
+               userTotal = 0;
+               $('#finalTotal').html(userTotal);
+           }
 
-// Appending random number to the randomNumber id in the html doc
-$('#randomNumber').html(Random);
+           // Selects a random number 19 - 120 to be shown at the start of the game 
+           var Random = Math.floor(Math.random() * 101) + 19
 
-//  Decaring variables for scoreboard
-$('#numWins').html(wins);
-$('#numLosses').html(losses);
+           // Appending random number to the randomNumber id in the html doc
+           $('#randomNumber').html(Random);
 
-// Sets up click for jewels
-$('#blue').on('click', function() {
-    userTotal = userTotal + blue;
-    console.log("New userTotal= " + userTotal);
-    $('#totalScore').html(userTotal);
+           //  Decaring variables for scoreboard
+           $('#numWins').html(wins);
+           $('#numLosses').html(losses);
 
-    //Sets win/lose conditions
-    if (userTotal == Random) {
-        huzzah();
-    } else if (userTotal > Random) {
-        loser();
-    }
-})
+           // Adds the wins to the userTotal
+           function huzzah() {
+               alert("You won!");
+               wins++;
+               $('#numWins').html(wins);
+               reset();
+           }
+           // Adds the losses to the userTotal
+           function loser() {
+               alert("You lose!");
+               losses++;
+               $('#numLosses').html(losses);
+               reset()
+           }
 
-$('#green').on('click', function() {
-    userTotal = userTotal + green;
-    console.log("New userTotal= " + userTotal);
-    $('#totalScore').html(userTotal);
+           // Sets up click for jewels
+           $('#blue').on('click', function() {
+               userTotal = userTotal + blue;
+               console.log("New userTotal= " + userTotal);
+               $('#totalScore').html(userTotal);
+           })
 
-    //sets win/lose conditions
-    if (userTotal == Random) {
-        yay();
-    } else if (userTotal > Random) {
-        loser();
-    }
-})
+           $('#green').on('click', function() {
+               userTotal = userTotal + green;
+               console.log("New userTotal= " + userTotal);
+               $('#totalScore').html(userTotal);
 
-$('#pink').on('click', function() {
-    userTotal = userTotal + pink;
-    console.log("New userTotal= " + userTotal);
-    $('#totalScore').html(userTotal);
+               //sets win/lose conditions
+               if (userTotal === Random) {
+                   huzzah();
+               } else if (userTotal > Random) {
+                   loser();
+               }
+           })
 
-    if (userTotal == Random) {
-        yay();
-    } else if (userTotal > Random) {
-        loser();
-    }
-})
+           $('#pink').on('click', function() {
+               userTotal = userTotal + pink;
+               console.log("New userTotal= " + userTotal);
+               $('#totalScore').html(userTotal);
 
-$('#yellow').on('click', function() {
-    userTotal = userTotal + yellow;
-    console.log("New userTotal= " + userTotal);
-    $('#totalScore').html(userTotal);
+               if (userTotal === Random) {
+                   huzzah();
+               } else if (userTotal > Random) {
+                   loser();
+               }
+           })
 
-    if (userTotal == Random) {
-        yay();
-    } else if (userTotal > Random) {
-        loser();
-    }
+           $('#yellow').on('click', function() {
+               userTotal = userTotal + yellow;
+               console.log("New userTotal= " + userTotal);
+               $('#totalScore').html(userTotal);
 
-    // Adds the wins to the userTotal
-    function huzzah() {
-        alert("You won!");
-        wins++;
-        $('#numWins').html(wins);
-        reset();
-    }
-    // Adds the losses to the userTotal
-    function loser() {
-        alert("You lose!");
-        losses++;
-        $('#numLosses').html(losses);
-        reset()
-    }
-});
+               if (userTotal === Random) {
+                   huzzah();
+               } else if (userTotal > Random) {
+                   loser();
+               }
+
+               //Sets win/lose conditions
+               if (userTotal === Random) {
+                   huzzah();
+               } else if (userTotal > Random) {
+                   loser();
+               }
+
+           });
+       });
